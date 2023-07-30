963cd40acb018e0623a48bfc7e2f393b3ac0a78a
mani pandit <manivpandit18@gmail.com>
2023-07-30 08:39:43 +0530

    initial commit

---
+++ src/components/Tours.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "./Card";
+
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <div className="container">
+      <div>
+        <h2 className="title"> Plan With Me</h2>
+      </div>
+
+      <div className="cards">
+        {tours.map((tour) => {
+          return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Tours;
